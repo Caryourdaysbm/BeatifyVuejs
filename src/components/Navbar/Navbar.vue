5ec96af9f0357dfccff478e8c4baefaaeabd19f7
unknown <adeyemioluwakayode73@gmail.com>
2023-07-26 17:54:44 +0100

    fixed hamburger

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -5,11 +5,15 @@
       </figure>
       <template v-if="!showExtension">
         <!-- Use PrimeIcons here -->
-        <i class="pi pi-align-right nav-icons" @click="showNavHandler"></i>
+        <div class="nav-icons">
+            <i class="pi pi-align-right nav-icons" @click="showNavHandler"></i>
+        </div>
       </template>
       <template v-else>
         <!-- Use PrimeIcons here -->
-        <i class="pi pi-times text-4xl text-black lg:hidden cursor-pointer" @click="closeNavHandler"></i>
+        <div class="text-4xl text-black lg:hidden cursor-pointer">
+            <i class="pi pi-times" @click="closeNavHandler"></i>
+        </div>
       </template>
       <ul :class="{ 'mobile-nav nav-link-container': showExtension, 'nav-link-container hidden-class': !showExtension }">
         <li class="nav-links" @click="closeNavHandler">
